f4657a6b76dd7d16e914a1da6d342f6ba0ba2239
lpost <lorenz.postner@talkpush.com>
2020-04-24 14:32:27 +0800

    cleaned up some files

--- scala/Demo.scala
+++
@@ -1,9 +0,0 @@
-object Demo {
-   def main(args: Array[String]) {
-      var x = 10;
-
-      if( x < 20 ){
-         println("This is if statement");
-      }
-   }
-}
--- scala/print_working_dir.scala
+++
@@ -1,6 +0,0 @@
-object Demo {
-  def main(args: Array[String]) {
-    var pwd = System.getProperty("user.dir")
-    println(pwd)
-  }
-}
